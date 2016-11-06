58010d32ddadb63aa9c8a36fd8ae13260a66db4d
Kayvan Mazaheri <KayvanMazaheri@GMail.com>
2016-11-06 21:02:00 +0330

    show server ip and port on uptime

--- index.js
+++ index.js
@@ -3,6 +3,13 @@
 const app = require('express')()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
+const nconf = require('nconf')
+
+nconf.env()
+nconf.argv()
+nconf.defaults({
+	'PORT' : 8080,
+})
 
 io.on('connection', function(socket){
 	console.log('connected')
@@ -15,8 +22,12 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname + '/chat.html')
 });
 
-http.listen(8080, function(err){
-	if(!err){
-		console.log('server is up and running.')
+http.listen(nconf.get('PORT'), function(err){
+	if(err){
+		console.log(err)
+	} else {
+		require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+			console.log('server is up and running on ' + add + ':' + nconf.get('PORT'))
+		})
 	}
 })
